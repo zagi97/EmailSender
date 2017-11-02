6fd175d704f9e2652d4f4600bd63b3e5d78a54b8
student <student@DESKTOP-QCKBJ2Q>
2017-11-02 10:07:17 +0100

    Email

---
+++ MailSender/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSender2;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailClient MailClient = new MailClient();
+            MailClient.SendEmail("kruno.zagar@vsmti.hr", " proba", "Boook");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MailSender/MailSender/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace MailSender
+{
+    public class MailClient
+    {
+        public string sSmtpClient = "smtp.office365.com";
+        public string sUsername = "racunarac@vsmti.hr";
+        public string sPassword = "Racunarstvo123";
+        public int nPort = 587;
+
+        public void SendEmail(string sEmail, string sSubject, string sBody)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient(sSmtpClient);
+            mail.From = new MailAddress(sUsername);
+            mail.To.Add(sEmail);
+            mail.Subject = sSubject;
+            mail.Body = sBody;
+            SmtpServer.Port = nPort;
+            SmtpServer.Credentials = new System.Net.NetworkCredential(sUsername, sPassword);
+            SmtpServer.EnableSsl = true;
+            SmtpServer.Send(mail);
+        }
+
+    }
+}
